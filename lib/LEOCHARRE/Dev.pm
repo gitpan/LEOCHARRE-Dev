494cb5ed3c945f350cf52c1ca9543854d7de2bf5
Leo Charre <leocharre@cpan.org>
2009-10-30 19:22:22 +0000

    Import of LEOCHARRE/LEOCHARRE-Dev-1.13 from CPAN.
    gitpan-cpan-distribution: LEOCHARRE-Dev
    gitpan-cpan-version:      1.13
    gitpan-cpan-path:         LEOCHARRE/LEOCHARRE-Dev-1.13.tar.gz
    gitpan-cpan-author:       LEOCHARRE
    gitpan-cpan-maturity:     released

--- lib/LEOCHARRE/Dev.pm
+++ lib/LEOCHARRE/Dev.pm
@@ -2,10 +2,11 @@ package LEOCHARRE::Dev;
 use strict;
 use Exporter;
 use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
-$VERSION = sprintf "%d.%02d", q$Revision: 1.12 $ =~ /(\d+)/g;
+$VERSION = sprintf "%d.%02d", q$Revision: 1.13 $ =~ /(\d+)/g;
 @EXPORT_OK = qw(is_pmdist ls_pmdist pmdist_guess_name pmdist_guess_version_from);
 @ISA = qw/Exporter/;
 %EXPORT_TAGS = ( 'all' => \@EXPORT_OK );
+
 
 $LEOCHARRE::Dev::DEBUG = 1;
 sub debug { print STDERR " - @_\n" if $LEOCHARRE::Dev::DEBUG; 1 }
